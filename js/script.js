bfe1eca0ca3f8f5f1d8daa5f31b14a987eebb4c9
Bruna Souza Chagas <bruna.chagass@hotmail.com>
2021-07-18 10:17:40 -0300

    master
    Criação da landing page de um e-commerce.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let dados = {
+    nome,
+    email,
+  }
+
+  let converteDados = JSON.stringify(dados);
+
+  localStorage.setItem('lead', converteDados);
+});
